66baf9d6787d42d58bbace96903a4abab8c963a0
Пользователь <@LAPTOP-U9PV1JFB>
2021-05-03 19:26:11 +0200

    01

---
+++ MyMeny.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace z23
+{
+    class MyMeny
+    {
+        public bool IsOpenTopTag { get; set; }
+        public bool isOpenBody { get; set; }
+        public StringBuilder ATributesBuilder { get; set; } = new StringBuilder();
+        public StringBuilder BodyBuilder { get; set; } = new StringBuilder();
+    }
+}
